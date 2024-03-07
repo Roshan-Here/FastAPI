6eefedf0a3c56fdd533fc9f91fdd138b6931fc3f
Muh Roshan <leoparmigiana@gmail.com>
2024-03-07 22:24:45 +0530

    setup unicorn into main.py

--- config/database.py
+++ config/database.py
@@ -1,9 +1,9 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
-load_dotenv()
+load_dotenv(find_dotenv())
 mongo_uri = os.environ.get("MONGO_URL")
 
 client = MongoClient(mongo_uri)
@@ -11,6 +11,13 @@ client = MongoClient(mongo_uri)
 db = client.todo_db
 
 collection_name = db['todo_collection']
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 
 def check_db_connection():
     try:
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from fastapi import FastAPI
-
-from routes.route import router
+import uvicorn
 
 
-app = FastAPI()
-
-app.include_router(router)
+if __name__ == "__main__":
+    uvicorn.run("app:app",host="127.0.0.1",port=8000,reload=False)
